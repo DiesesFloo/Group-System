af82f369b4d83e337147d1cd1b71af8a1a93025f
Floo <fmanice07@gmail.com>
2023-07-16 19:37:47 +0200

    feat: Made UUIDFetcher async

--- src/main/java/net/playlegend/spigot/groupsystem/mojang/UUIDFetcher.java
+++ src/main/java/net/playlegend/spigot/groupsystem/mojang/UUIDFetcher.java
@@ -3,19 +3,28 @@ package net.playlegend.spigot.groupsystem.mojang;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.util.UUIDTypeAdapter;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UUIDFetcher {
 
-    private static final String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/%s?at=%d";
-    private static Gson gson = new GsonBuilder().registerTypeAdapter(UUID.class, new UUIDTypeAdapter()).create();
+    static final ExecutorService pool = Executors.newCachedThreadPool();
 
-    private UUID id;
+    static final String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/%s?at=%d";
+    static final Gson gson = new GsonBuilder().registerTypeAdapter(UUID.class, new UUIDTypeAdapter()).create();
+
+    UUID id;
 
     public static UUID getUUID(String name){
         if (name == null) return null;
@@ -25,7 +34,22 @@ public class UUIDFetcher {
         try {
             HttpURLConnection connection = (HttpURLConnection) new URL(String.format(UUID_URL, name, System.currentTimeMillis()/1000)).openConnection();
             connection.setReadTimeout(5000);
-            UUIDFetcher data = gson.fromJson(new BufferedReader(new InputStreamReader(connection.getInputStream())), UUIDFetcher.class);
+
+            CompletableFuture<UUIDFetcher> dataFuture = CompletableFuture.supplyAsync(
+                    () -> {
+                        try {
+                            return gson.fromJson(new BufferedReader(new InputStreamReader(connection.getInputStream())), UUIDFetcher.class);
+                        } catch (IOException e) {
+                            return null;
+                        }
+                    }, pool
+            );
+
+            UUIDFetcher data = dataFuture.get();
+
+            if (data == null) {
+                return null;
+            }
 
             return data.id;
         }catch (Exception e){
